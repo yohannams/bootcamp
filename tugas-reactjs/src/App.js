82540bb0bc9f010c802bfae74a9939a582e89aed
yohannams <yo.hanna.ms.1993@gmail.com>
2023-02-08 21:55:31 +0700

    tugas 8

--- tugas-reactjs/src/App.js
+++ tugas-reactjs/src/App.js
@@ -1,14 +1,15 @@
+import React from 'react';
+import Tugas7 from './tugas7/tugas7';
+import Tugas8 from './tugas8/tugas8';
 import './App.css';
+
 
 function App() {
   return (
-    <div>
-      <div className="container">
-        <h1>Hi, Saya Yohanna</h1>
-        <hr></hr>
-        <label>Saya dari Batch 42 sudah berhasil menginstall React</label>
-      </div>
-    </div>
+    <>
+      <Tugas7/>
+      <Tugas8 name="Yohanna" batch="42" email="yo.hanna.ms.1993@gmail.com"/>
+    </>
   );
 }
 
