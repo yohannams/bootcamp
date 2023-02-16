3ceff29a641a6b8cd0b9af69b9a9b9fa87ec43a5
yohannams <yo.hanna.ms.1993@gmail.com>
2023-02-16 23:28:48 +0700

    tugas 14 final

--- tugas-reactjs/src/App.js
+++ tugas-reactjs/src/App.js
@@ -7,7 +7,6 @@ import Tugas10 from './tugas10/tugas10';
 import Tugas11 from './tugas11/tugas11';
 import Tugas12 from './tugas12/tugas12';
 import Tugas13 from './tugas13/tugas13';
-import Tugas14 from './tugas14/tugas14';
 import './App.css';
 
 function App() {
@@ -20,7 +19,7 @@ function App() {
         <Tugas10/> 
         <Tugas11/> 
         <Tugas12/> */}
-        <Tugas14/>
+        <Tugas13/>
       </GlobalProvider>
     </>
   );
--- tugas-reactjs/src/tugas13/tugas13.js
+++ tugas-reactjs/src/tugas13/tugas13.js
@@ -1,118 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
+import { GlobalContext } from '../context/GlobalContext';
 
 const Tugas13 = () => {
-    const [data,setData] = useState(null)
-    const [input,setInput] = useState({
-        name: "",
-        course: "",
-        score: 0
-    })
+    const {state, handleFunction} = useContext(GlobalContext)
+    const {
+        data,setData,
+        input,setInput,
+        fetchStatus,setFetchStatus,
+        currentId, setCurrentId
+    } = state
 
-    const handleChange = (event) => {
-        let name = event.target.name
-        let value = event.target.value
-
-        if(name === "name"){         
-            setInput({...input,name:value})
-        }else if(name === "course"){
-            setInput({...input,course:value})
-        }else if(name === "score"){
-            setInput({...input,score:value})
-        }
-    }
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
-        let {name} = input
-        let {course} = input
-        let {score} = input
-
-        if(currentId === -1){
-            axios.post('https://backendexample.sanbercloud.com/api/student-scores', {name,course,score})
-            .then((res)=>{
-                setFetchStatus(true)
-            })
-            .catch((err)=>{
-    
-            })    
-        }else{
-            axios.put(`https://backendexample.sanbercloud.com/api/student-scores/${currentId}`,{name,course,score})
-            .then((res)=>{
-                setFetchStatus(true)
-            })
-            .catch((err)=>{
-
-            })
-        }
-
-        setInput({
-            name:"",
-            course:"",
-            score:0
-        })
-        setCurrentId(-1)
-    }
-
-    const handleEdit = (event)=>{
-        let idData = parseInt(event.target.value)
-        axios.get(`https://backendexample.sanbercloud.com/api/student-scores/${idData}`)
-        .then((res)=>{
-            let result = res.data;
-            setInput({
-                name: result.name,
-                course: result.course,
-                score: result.score
-            })
-            setCurrentId(result.id)
-        })
-        .catch((error)=>{
-            console.log(error)
-        })
-    }
-
-    const handleDelete = (event)=>{
-        let idData = parseInt(event.target.value)
-        axios.delete(`https://backendexample.sanbercloud.com/api/student-scores/${idData}`)
-        .then((res)=>{
-            setFetchStatus(true)
-        })
-        .catch((error)=>{
-            console.log(error)
-        })
-    }
-
-    const [fetchStatus,setFetchStatus] = useState(true)
-    const [currentId, setCurrentId] = useState(-1)
+    const {
+        handleIndexScore,
+        handleDelete,
+        handleEdit,
+        handleSubmit,
+        handleChange,
+        fetchData
+    } = handleFunction
 
     useEffect(()=>{
         if(fetchStatus === true){
-            axios.get('https://backendexample.sanbercloud.com/api/student-scores')
-            .then((res)=>{
-                let result = res.data;
-                setData([...result])
-            })
-            .catch((error)=>{
-                console.log(error)
-            })
-            setFetchStatus(false)
+            fetchData()
         }
-    },[fetchStatus,setFetchStatus])
+    },[fetchStatus,setFetchStatus,fetchData])
 
-    const handleIndexScore = (nilai) => {
-      if(nilai >= 80){
-        return "A"
-      }else if(nilai >= 70 && nilai < 80){
-        return "B"
-      }else if(nilai >= 60 && nilai < 70){
-        return "C"
-      }else if(nilai >= 50 && nilai < 60){
-        return "D"
-      }else{
-        return "E"
-      }
-    }
 
   return (
     <>
