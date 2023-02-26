7dd648170184784abe2a44dfba4eb573a094aed5
yohannams <yo.hanna.ms.1993@gmail.com>
2023-02-26 16:02:53 +0700

    final project

--- tugas-reactjs/src/App.js
+++ tugas-reactjs/src/App.js
@@ -1,23 +1,51 @@
 import React from 'react';
 import { GlobalProvider } from './context/GlobalContext';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Cookies from "js-cookie";
 import Navbar from './components/navbar';
 import Tugas15Form from './tugas15/Tugas15Form';
 import Tugas15List from './tugas15/Tugas15List';
 import Tugas7 from './tugas7/tugas7';
+import Home from './components/home';
+import Login from './components/login';
 import './App.css';
+import Layout from './layout/layout';
+
 
 function App() {
+
+  const LoginRoute = (props) => {
+    if(Cookies.get('token') === undefined){
+      return props.children
+    }else if(Cookies.get('token') !== undefined){
+      return <Navigate to='/'/>
+    }
+    
+  }
+
   return (
     <>
       <BrowserRouter>
         <GlobalProvider>
-          <Navbar/>
+          {/* <Navbar/> */}
           <Routes>
-          <Route path='/' element={<Tugas7/>} />
+            <Route path='/' element={
+              <Layout>
+                <Home />
+              </Layout>
+            } />
+
+            <Route path='/login' element={
+                <LoginRoute>
+                 <Layout>
+                  <Login />
+                 </Layout>
+                </LoginRoute>
+            } />
+            {/* <Route path='/' element={<Tugas7/>} />
             <Route path='/tugas15' element={<Tugas15List/>} />
             <Route path='/create' element={<Tugas15Form/>} />
-            <Route path='/edit/:IdData' element={<Tugas15Form/>} />
+            <Route path='/edit/:IdData' element={<Tugas15Form/>} /> */}
           </Routes>
         </GlobalProvider>
       </BrowserRouter>
--- tugas-reactjs/src/components/navbar.js
+++ tugas-reactjs/src/components/navbar.js
@@ -1,7 +1,10 @@
 import { React } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
 
 const Navbar = () => {
+let navigate = useNavigate();
+
   return (
     <div style={{margin:'auto',width:'80%'}}>
         <div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
@@ -9,12 +12,27 @@ const Navbar = () => {
                 <li class="mr-2">
                     <Link to="/" class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Home</Link>
                 </li>
-                <li class="mr-2">
+                {!Cookies.get('token') && 
+                    <li class="mr-2">
+                        <Link to="/login" class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Login</Link>
+                    </li>
+                }
+                {Cookies.get('token') && 
+                    <li class="mr-2">
+                        <Link to="/" class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" onClick={
+                            () => {
+                                Cookies.remove('token')
+                                navigate('/login')
+                            }
+                        }>Logout</Link>
+                    </li>
+                }
+                {/* <li class="mr-2">
                     <Link to="/tugas15" class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Materi</Link>
                 </li>
                 <li class="mr-2">
                     <Link to="/create" class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Create</Link>
-                </li>
+                </li> */}
             </ul>
         </div>
     </div>
